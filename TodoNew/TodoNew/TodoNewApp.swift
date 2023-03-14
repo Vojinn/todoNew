e68fe6d69f11bec375cb88ff563711dd37f748a2
Vojin <dvojinovic38@gmail.com>
2023-03-14 22:12:56 +0100

    Initial commit

---
+++ TodoNew/TodoNew/TodoNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoNewApp.swift
+//  TodoNew
+//
+//  Created by Dusan Vojinovic on 14.3.23..
+//
+
+import SwiftUI
+
+@main
+struct TodoNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
